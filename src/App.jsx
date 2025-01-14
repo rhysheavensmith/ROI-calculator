8f55d4fa122af81f6568bb8995464b9405427c90
Rhys Heaven-Smith <rhysheavensmith@gmail.com>
2025-01-15 04:44:45 +1300

    removed bg color and changed the root div id to calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import CalculatorTabs from './components/CalculatorTabs';
 
 const App = () => {
 	return (
-		<div className='flex min-h-screen justify-center items-center bg-[#7ab5ff] py-4'>
+		<div className='flex min-h-screen justify-center items-center py-4'>
 			<CalculatorTabs />
 		</div>
 	);
